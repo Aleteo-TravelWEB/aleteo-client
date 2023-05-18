91a572b45eeff77677c186929243278770fcd5b1
lkm454545 <lkm454545@naver.com>
2023-05-19 08:57:03 +0900

    Refactor: 공지사항 댓글 수정

--- src/components/answer/AnswerList.vue
+++ src/components/answer/AnswerList.vue
@@ -1,26 +1,26 @@
 <template>
-<!-- 알았으니까
+  <!-- 알았으니까
 이제 b-어쩌구 있는거면
 뭔지 제대로 알고 사용을 하셔야합니다.
 -->
   <div id="container">
     <div>댓글 목록</div>
-      <div v-for="answer in answers" :key="answer.id">
-        <b-card class="col-12">
-          <b-card-text>{{ answer.content }}</b-card-text>
-        </b-card>
-      </div>
+    <div v-for="answer in answers" :key="answer.id">
+      <b-card class="col-12">
+        <b-card-text>{{ answer.content }}</b-card-text>
+      </b-card>
     </div>
+  </div>
 </template>
 
 <script>
 import { listAnswer } from "@/api/answer";
 
 export default {
-  name: 'AnswerList',
+  name: "AnswerList",
   data() {
     return {
-      answers: []
+      answers: [],
     };
   },
   created() {
@@ -28,14 +28,14 @@ export default {
     console.log(param);
     listAnswer(
       param,
-      ({data}) => {
+      ({ data }) => {
         console.log(data);
         this.answers = data;
       },
       (error) => {
         console.log(error);
       }
-    )
+    );
   },
   methods: {},
 };
--- src/components/answer/AnswerWrite.vue
+++ src/components/answer/AnswerWrite.vue
@@ -2,31 +2,65 @@
   <div id="answerbox">
     <div>답변 등록</div>
     <form id="answer" @submit="submit" class="form-flex">
-      <input type="text" v-model="answer.content" class="input-field">
+      <input type="text" v-model="answer.content" class="input-field" />
       <b-button type="submit" variant="primary" class="m-1">등록</b-button>
     </form>
   </div>
 </template>
 
 <script>
+import { writeAnswer } from "@/api/answer";
 
 export default {
-  name: 'AnswerWrite',
+  name: "AnswerWrite",
   data() {
     return {
       answer: {
         id: 0,
         content: "",
         userId: "",
-        boardId: this.$route.params.id,
-      }
+        boardId: "",
+      },
     };
   },
   created() {
     console.log(this.$route.params.id);
-    
+    console.log(this.$route);
   },
-  methods: { 
+  methods: {
+    submit(event) {
+      event.preventDefault();
+
+      let err = true;
+      let msg = "";
+
+      err &&
+        !this.article.content &&
+        ((msg = "내용 입력해주세요"), (err = false), this.$refs.content.focus());
+
+      if (!err) alert(msg);
+      else this.writeAnswer();
+    },
+    writeAnswer() {
+      let param = {
+        content: this.answer.content,
+        boardId: this.$route.params.id,
+        userId: this.userInfo.userId,
+      };
+      writeAnswer(
+        param,
+        ({ data }) => {
+          let msg = "등록 처리시 문제가 발생 했습니다.";
+          if (data === "success") {
+            msg = "등록 완료";
+          }
+          alert(msg);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
   },
 };
 </script>
@@ -55,5 +89,4 @@ export default {
   padding: 5px;
   margin-right: 5px;
 }
-
 </style>
