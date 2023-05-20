b05fad826d9292d0b7ba9b3e21b0b82e20249f59
lkm454545 <lkm454545@naver.com>
2023-05-21 02:12:34 +0900

    Refactor: 핫플레이스 목록 출력 기능

--- src/components/hotplace/HotplaceList.vue
+++ src/components/hotplace/HotplaceList.vue
@@ -2,20 +2,39 @@
   <b-container>
     <div>핫플레이스 어쩌고 저쩌구</div>
     <button @click="movetoRegist" id="registbutton" class="styled-button">등록하러 가기</button>
+    <b-row>
+      <b-col cols="4" v-for="hotplace in pagination" :key="hotplace.id">
+        <b-card
+          :title="hotplace.title"
+          img-alt="Image"
+          img-top
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text> #{{ hotplace.tag1 }} #{{ hotplace.tag2 }} </b-card-text>
+          <b-button href="#" variant="primary">더 보기</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="hotplaces.length"
+      :per-page="perPage"
+      align="center"
+      class="mt-4"
+    />
   </b-container>
 </template>
 
 <script>
 import { listHotplace } from "@/api/hotplace";
 
-
 export default {
-  name: 'HotplaceList',
-  components: {
-  },
+  name: "HotplaceList",
+  components: {},
   data() {
     return {
-      hotpalces: [],
+      hotplaces: [],
       currentPage: 1,
       perPage: 9,
     };
@@ -23,13 +42,19 @@ export default {
   created() {
     listHotplace(
       ({ data }) => {
-        this.hotpalces = data;
-        console.log(data);
+        this.hotplaces = data;
       },
       (error) => {
         console.log(error);
       }
-    )
+    );
+  },
+  computed: {
+    pagination() {
+      const startIndex = (this.currentPage - 1) * this.perPage;
+      const endIndex = startIndex + this.perPage;
+      return this.hotplaces.slice(startIndex, endIndex);
+    },
   },
   methods: {
     movetoRegist() {
@@ -37,14 +62,25 @@ export default {
       this.$router.push({
         name: "hotplacewrite",
       });
-    }
+    },
+  },
+  convertImageToBase64(imageFile) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(imageFile);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  },
+  makerealUrl(imageFile) {
+    console.log(this.convertImageToBase64(imageFile));
   },
 };
 </script>
 
 <style scoped>
 .styled-button {
-  background-color: #4CAF50;
+  background-color: #4caf50;
   border: none;
   color: white;
   padding: 8px 16px;
