7489c5ed2c5a626e1546bd5f5ad823a40df15e9c
tngus2sh <https://github.com/tngus2sh>
2023-05-23 01:29:24 +0900

    Feat: 메인화면 수정

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="background"></div>
+  <div class="background">
+    <div class="title d-flex flex-row justify-content-center" style="width: 100%">
+      <h1 class="title-content">ALETEO</h1>
+    </div>
+    <div class="d-flex flex-row justify-content-center mb-2 search" style="width: 100%">
+      <div class="today mx-3 p-2 shadow px-5" @click="moveAttraction()">
+        오늘의 관광지는? <b-icon class="px-3" icon="search" animation="fade" />
+      </div>
+    </div>
+
+    <div class="content">
+      <div>여행 계획 목록 출력</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,7 +25,11 @@ export default {
     };
   },
   created() {},
-  methods: {},
+  methods: {
+    moveAttraction() {
+      this.$router.push({ name: "attraction" });
+    },
+  },
 };
 </script>
 
@@ -21,9 +38,45 @@ export default {
   height: 100vh;
   overflow: hidden;
   margin: 0;
-  background-image: url("@/assets/img/background4.jpg");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  position: relative;
+  background-image: url("@/assets/img/background4.jpg");
+}
+
+.title {
+  position: absolute;
+  top: 200px;
+}
+
+.title-content {
+  font-size: 60px;
+  color: black;
+}
+
+.search {
+  position: absolute;
+  top: 300px;
+}
+
+.today {
+  background: #ffffff2b;
+  width: 300px;
+  border: 2px solid #c1c1c1;
+  border-radius: 10px;
+}
+
+.today:hover {
+  cursor: pointer;
+  border: 4px solid #c1c1c1;
+  box-shadow: 2px 3px 10px 0px;
+}
+
+.content {
+  background: #ffffff;
+  width: 100%;
+  top: 700px;
+  position: absolute;
 }
 </style>
